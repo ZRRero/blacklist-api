50bdb3aea1d34c0d30e02d32efdafd1b6ad3442a
David Useche <david-use@hotmail.com>
2023-04-10 18:03:41 -0500

    Added missing methods

--- pkg/clients/blacklist_dynamo_client.go
+++ pkg/clients/blacklist_dynamo_client.go
@@ -60,7 +60,19 @@ func (receiver *BlacklistDynamoClient) GetRecordBatch(requests []*blacklist.Blac
 	if len(requests) > 25 {
 		return nil, errors.New("ids list has more than BlacklistDynamoClient max batch (25)")
 	}
-
+	input := &dynamodb.BatchGetItemInput{
+		RequestItems: receiver.getBatchRequestFromRequest(requests),
+	}
+	result, err := receiver.client.BatchGetItem(input)
+	if err != nil {
+		return nil, err
+	}
+	dynamoRecords := result.Responses[receiver.table]
+	records, err := receiver.parseDynamoRecords(dynamoRecords)
+	if err != nil {
+		return nil, err
+	}
+	return records, nil
 }
 
 func (receiver *BlacklistDynamoClient) getBatchRequestFromRequest(requests []*blacklist.BlacklistRecordDto) map[string]*dynamodb.KeysAndAttributes {
@@ -91,4 +103,98 @@ func (receiver *BlacklistDynamoClient) parseDynamoRecords(dynamoRecords []map[st
 		records = append(records, record)
 	}
 	return records, nil
+}
+
+//Save
+
+func (receiver *BlacklistDynamoClient) SaveRecord(record *models.Record) (*models.Record, error) {
+	input := &dynamodb.PutItemInput{
+		TableName: &receiver.table,
+		Item:      record.ToDynamoItem(),
+	}
+	_, err := receiver.client.PutItem(input)
+	if err != nil {
+		return nil, err
+	}
+	return record, nil
+}
+
+func (receiver *BlacklistDynamoClient) SaveBatchRecords(records []*models.Record) ([]*models.Record, error) {
+	if len(records) > 25 {
+		return nil, errors.New("ids list has more than BlacklistDynamoClient max batch (25)")
+	}
+	input := &dynamodb.BatchWriteItemInput{
+		RequestItems: receiver.getWriteBatchRequestFromModel(records),
+	}
+	result, err := receiver.client.BatchWriteItem(input)
+	if err != nil {
+		return nil, err
+	}
+	for len(result.UnprocessedItems) != 0 {
+		input = &dynamodb.BatchWriteItemInput{
+			RequestItems: result.UnprocessedItems,
+		}
+		result, err = receiver.client.BatchWriteItem(input)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return records, nil
+}
+
+func (receiver *BlacklistDynamoClient) getWriteBatchRequestFromModel(records []*models.Record) map[string][]*dynamodb.WriteRequest {
+	items := make(map[string][]*dynamodb.WriteRequest)
+	requests := make([]*dynamodb.WriteRequest, 0, len(records))
+	for _, record := range records {
+		requests = append(requests, &dynamodb.WriteRequest{PutRequest: &dynamodb.PutRequest{Item: record.ToDynamoItem()}})
+	}
+	items[receiver.table] = requests
+	return items
+}
+
+//Delete
+
+func (receiver *BlacklistDynamoClient) DeleteRecord(id *string) error {
+	input := &dynamodb.DeleteItemInput{
+		TableName: &receiver.table,
+		Key:       map[string]*dynamodb.AttributeValue{"id": {S: id}},
+	}
+	_, err := receiver.client.DeleteItem(input)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (receiver *BlacklistDynamoClient) DeleteBatchRecords(ids []*string) error {
+	if len(ids) > 25 {
+		return errors.New("ids list has more than BlacklistDynamoClient max batch (25)")
+	}
+	input := &dynamodb.BatchWriteItemInput{
+		RequestItems: receiver.getDeleteBatchRequestFromIds(ids),
+	}
+	result, err := receiver.client.BatchWriteItem(input)
+	if err != nil {
+		return err
+	}
+	for len(result.UnprocessedItems) != 0 {
+		input = &dynamodb.BatchWriteItemInput{
+			RequestItems: result.UnprocessedItems,
+		}
+		result, err = receiver.client.BatchWriteItem(input)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (receiver *BlacklistDynamoClient) getDeleteBatchRequestFromIds(ids []*string) map[string][]*dynamodb.WriteRequest {
+	items := make(map[string][]*dynamodb.WriteRequest)
+	requests := make([]*dynamodb.WriteRequest, 0, len(ids))
+	for _, id := range ids {
+		requests = append(requests, &dynamodb.WriteRequest{DeleteRequest: &dynamodb.DeleteRequest{Key: map[string]*dynamodb.AttributeValue{"id": {S: id}}}})
+	}
+	items[receiver.table] = requests
+	return items
 }
